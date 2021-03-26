d918726e0bede003f32b5a6dfac2966f0ab6fa5c
yongjun <yes1312@naver.com>
2021-03-26 14:18:07 +0900

    데이터 갖고 놀기 - 데이터 마무리

---
+++ 데이터 마무리/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 데이터_마무리
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int a;
+            //a = 100;
+
+            //int b;
+            //b = a;
+
+            //a += 1; // a = a+1
+            //a -= 1;
+            //a *= 1;
+            //a &= 1;
+            //a <<= 1;
+
+            //연산의 우선 순위
+            // 1. ++ -- 
+            // 2. * / % 
+            // 3. + -
+            // 4. << >> 
+            // 5. < > ...
+            // 6. == !=
+            // 7. &
+            // 8. ^
+            // 9. |
+            // .. 
+            // 괄호를 써서 우선 순위 지정 가능
+
+            // var 변수 자동적으로 자료형을 할당
+            // 많이 사용하는 것은 좋지 않음
+            //var num = 3;
+            //var num2 = "hello";
+
+
+        }
+    }
+}
