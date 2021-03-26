8095b321d970b7f31f34eed978acce809dab6fa4
yongjun <yes1312@naver.com>
2021-03-26 12:23:48 +0900

    day 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,9 +8,17 @@ namespace ConsoleApp1
 {
     class Program
     {
+        // 주석은 C++이랑 같음
+        /* 이것도 마찬가지 */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 프로그램 = 데이터 + 로직
+            // 정수형 데이터는 int
+            // 정수형 변수 hp를 선언
+            int hp;
+            // hp에 숫자 100을 할당
+            hp = 100;
+            Console.WriteLine("Hello number! ! {0}", hp);
         }
     }
 }
