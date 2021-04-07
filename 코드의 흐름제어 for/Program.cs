174f2b7f8cfd0322a38a7fd1ffa0565fdca4409d
yongjun <yes1312@naver.com>
2021-04-08 02:46:10 +0900

    코드의 흐름제어 - for

---
+++ 코드의 흐름제어 for/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 코드의_흐름제어_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (초기화식; 조건식; 반복식)
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
