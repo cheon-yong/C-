1f8c0a4f45aa35cfd667506d3543e7e0d10d634a
yongjun <yes1312@naver.com>
2021-04-08 02:28:34 +0900

    코드의 흐름제어 - while

---
+++ 코드의 흐름제어 while/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 코드의_흐름제어_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // while 반복문
+            int count = 5;
+            while (count > 0)
+            {
+                Console.WriteLine("Hello World");
+                count--;
+            }
+            string answer; 
+            do
+            {
+                Console.WriteLine("Is that true?");
+                answer = Console.ReadLine();
+            } while (answer != "y");
+        }
+    }
+}
