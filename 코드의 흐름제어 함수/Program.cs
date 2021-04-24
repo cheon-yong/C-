4135204483097479a211f5f46f19f8ddd80dd8bf
yongjun <yes1312@naver.com>
2021-04-25 01:33:37 +0900

    코드의 흐름제어 - 함수

---
+++ 코드의 흐름제어 함수/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 코드의_흐름제어_함수
+{
+    class Program
+    {
+        // 메소드 함수 method function ...
+        /* 
+         * 한정자 반환형식 이름 (매개변수)
+           {
+           }
+         */
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        static void Main(string[] args)
+        {
+            HelloWorld();
+        }
+    }
+}
