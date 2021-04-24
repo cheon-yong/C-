33b2824eee78811d842d71451f603d2d51642494
yongjun <yes1312@naver.com>
2021-04-25 02:04:16 +0900

    코드의 흐름 제어 - ref, out

---
+++ 코드의 흐름제어 ref, out/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace 코드의_흐름제어_ref__out
+{
+    class Program
+    {
+        static void Divide(int a, int b, out int result1, out int result2)
+        {
+            result1 = a / b;
+            result2 = a % b;
+        }
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void AddOne(ref int number)
+        {
+            number += 1;
+        }
+
+        static int AddOne2(int number)
+        {
+            return number + 1;
+        }
+        static void Main(string[] args)
+        {
+            int a = 100;
+            AddOne(ref a);
+            Console.WriteLine(a);
+
+            //같은 동작을 하지만 AddOne2식으로 구현하는 것이 유리
+            int b = AddOne2(a);
+            Console.WriteLine(b);
+
+            //그러나 참조하는 것을 사용해야할 경우가 있음 (ex. swap)
+            int num1 = 1;
+            int num2 = 2;
+            Swap(ref num1, ref num2);
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+
+            int number1 = 10;
+            int number2 = 3;
+
+            int result1;
+            int result2;
+
+            Divide(number1, number2, out result1, out result2);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            //out을 사용
+
+        }
+    }
+}
