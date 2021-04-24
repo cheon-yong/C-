0f6edf302d5799b0f8b14a9525cf0b0046acb133
yongjun <yes1312@naver.com>
2021-04-25 01:43:28 +0900

    코드의 흐름제어 - 함수

--- 코드의 흐름제어 함수/Program.cs
+++ 코드의 흐름제어 함수/Program.cs
@@ -19,9 +19,28 @@ namespace 코드의_흐름제어_함수
             Console.WriteLine("Hello World");
         }
 
+        static int Add(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+
+        static void Addone(ref int number)
+        {
+            number = number + 1;
+        }
         static void Main(string[] args)
         {
+            // int a, int b
+            // main 함수에서 a, b를 선언해도 Add 함수의 a,b와는 상관 없음
+            // a의 값을 Add one 이라는 함수를 통해서 100이 증가하지 않음
+            // 증가하는 것을 원하면 ref라는 키워드를 사용해야함
+            int a = 100;
             HelloWorld();
+            int result = Add(4, 5);
+            Console.WriteLine(result);
+            Addone(ref a);
+            Console.WriteLine(a);
         }
     }
 }
