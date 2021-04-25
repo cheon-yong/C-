faf5c94634b71946ef3975098a21c6370897e521
yongjun <yes1312@naver.com>
2021-04-25 22:56:47 +0900

    TextRPG - 디버깅 기초

--- TextRPG 디버깅 기초/Program.cs
+++ TextRPG 디버깅 기초/Program.cs
@@ -7,7 +7,7 @@ namespace TextRPG_디버깅_기초
         // 디버깅은 많은 연습이 필요하고 
         // 프로그램을 작성할 때 꼭 필요한 것
         // 브레이크 포인트를 사용하는 것을 꼭 익혀둘 것
-        //
+        // 값들을 출력해서 확인하는 방법도 있지만 비추천
         static void Print(int value)
         {
             Console.WriteLine(value);
