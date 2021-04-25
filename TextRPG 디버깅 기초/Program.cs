09637a5622630b635fc8e0ee6ae2c73d2ba899f0
yongjun <yes1312@naver.com>
2021-04-25 22:44:58 +0900

    TextRPG - 디버깅 기초

---
+++ TextRPG 디버깅 기초/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TextRPG_디버깅_기초
+{
+    class Program
+    {
+        // 디버깅은 많은 연습이 필요하고 
+        // 프로그램을 작성할 때 꼭 필요한 것
+        // 브레이크 포인트를 사용하는 것을 꼭 익혀둘 것
+        //
+        static void Print(int value)
+        {
+            Console.WriteLine(value);
+        }
+        static int AddAndPrint(int a, int b)
+        {
+            int ret = a + b;
+            Print(ret);
+            return ret;
+        }
+        static void Main(string[] args)
+        {
+            int ret = AddAndPrint(10, 20);
+            ret = ret + 1;
+            Console.WriteLine(ret);
+        }
+    }
+}
