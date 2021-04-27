8ec72ebbac561f34d6ba7417f52d4eab7388e913
yongjun <yes1312@naver.com>
2021-04-27 21:41:15 +0900

    객체지향 여행 - 복사(값)와 참조

--- 객체지향 여행 객체지향의 시작/Program.cs
+++ 객체지향 여행 객체지향의 시작/Program.cs
@@ -9,6 +9,7 @@ namespace 객체지향_여행_객체지향의_시작
     // 속성 : hp, attack
     // 기능 : Move, Attack, Die
 
+    // 참조 복사
     class Knight
     {
         public int hp;
@@ -24,9 +25,33 @@ namespace 객체지향_여행_객체지향의_시작
             Console.WriteLine("Knight Attack");
         }
 
+        public Knight Clone()
+        {
+            Knight knight = new Knight();
+            knight.hp = hp;
+            knight.attack = attack;
+            return knight;
+        }
+
+    }
+
+    // 얕은 복사
+    struct Mage
+    {
+        public int hp;
+        public int attack;
     }
     class Program
     {
+        static void KillMage(Mage mage)
+        {
+            mage.hp = 0;
+        }
+
+        static void KillKnight(Knight knight)
+        {
+            knight.hp = 0 ;
+        }
 
         // 객체는 new 키워드를 반드시 사용해야함
         static void Main(string[] args)
@@ -34,9 +59,33 @@ namespace 객체지향_여행_객체지향의_시작
             Knight knight = new Knight();
             knight.hp = 100;
             knight.attack = 100;
+           
+            /*
+            Mage mage;
+            mage.hp = 100;
+            mage.attack = 10;
+
+            Mage mage2 = mage;
+            mage2.hp = 0;
 
             knight.Move();
             knight.Attack();
+            KillKnight(knight);
+            KillMage(mage);
+            */
+
+            //아래와 같이하면 knight와 knight2는 같은 것
+            //Knight knight2 = knight;
+            //knight.hp = 0;
+
+            //아래와 같은 방법들이 있음
+            //Knight knight2 = new Knight();
+            //knight2.hp = 200;
+            //knight2.attack = 100;
+
+
+            Knight knight2 = knight.Clone();
+
         }
     }
 }
