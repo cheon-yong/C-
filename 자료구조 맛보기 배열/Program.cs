20135a80be2759d47a78ceb20575aa3e1ad1f83a
yongjun <yes1312@naver.com>
2021-05-03 17:21:59 +0900

    자료구조 맛보기 - 연습문제

---
+++ 자료구조 맛보기 배열/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace 자료구조_맛보기_배열
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 배열
+            // 크기와 입력한 숫자만큼 입력해야함
+            //int[] scores = new int[] {10, 20, 30, 40, 50 };
+            //int[] scores = { 10, 20, 30, 40, 50 };
+            int[] scores = new int[5] { 10, 20, 30, 40, 50 };
+
+            //배열은 참조타입
+            //index는 0부터 시작
+            //scores[0] = 10;
+            //scores[1] = 20;
+            //scores[2] = 30;
+            //scores[3] = 40;
+            //scores[4] = 50;
+
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine(scores[i]);
+            }
+
+
+            foreach (int score in scores)
+            {
+                Console.WriteLine(score);
+            }
+        }
+    }
+}
---
+++ 자료구조 맛보기 연습문제/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace 자료구조_맛보기_연습문제
+{
+    class Program
+    {
+        static int GetHighestScore(int[] scores)
+        {
+            if (scores == null)
+                return 0;
+
+            int res = 0;
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (res < scores[i])
+                    res = scores[i];
+            }
+
+            return res;
+        }
+        static int GetAverageScore(int[] scores)
+        {
+            if (scores == null)
+                return 0;
+
+            int sum = 0;
+            int res;
+            for (int i = 0; i < scores.Length; i++)
+                sum += scores[i];
+            res = sum / scores.Length;
+
+            return res;
+        }
+
+        static int GetIndexOf(int[] scores, int value)
+        {
+            if (scores == null)
+                return -1;
+            for (int i = 0; i<scores.Length; i++)
+            {
+                if (value == scores[i])
+                    return i;
+            }
+
+            return -1;
+        }
+
+        static void Sort(int[] scores)
+        {
+            for (int i=0; i<scores.Length; i++)
+            {
+                for (int j=i; j<scores.Length; j++)
+                {
+                    if (scores[i] > scores[j])
+                    {
+                        int temp = scores[i];
+                        scores[i] = scores[j];
+                        scores[j] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            //0 ~ 100
+            int[] scores = new int[] {40, 15, 23, 50, 70, 98};
+            Console.WriteLine(GetHighestScore(scores));
+            Console.WriteLine(GetAverageScore(scores));
+            Console.WriteLine(GetIndexOf(scores, 23));
+            Sort(scores);
+            foreach (int score in scores)
+                Console.Write($"{score} ");
+        }
+    }
+}
