6ba924e805565aedfa60c6c72f2be7b0eb716fb6
yongjun <yes1312@naver.com>
2021-05-04 01:45:27 +0900

    알아두면 유용한 기타 문법 - Event(이벤트)

---
+++ 알아두면 유용한 기타 문법 Event(이벤트)/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 알아두면_유용한_기타_문법_Event_이벤트_
+{
+    class InputManager
+    {
+        public delegate int OnInputKey(int value);
+        public event OnInputKey InputKey;
+
+        //event를 호출하고 싶으면 구독하면 됨
+        //Youtube의 구독과 유사한 기능
+        //사용자가 입력하는 것을 캐치하는 역할
+        public void Update()
+        {
+            if (Console.KeyAvailable == false)
+                return;
+
+            ConsoleKeyInfo info =  Console.ReadKey();
+            if (info.Key == ConsoleKey.A)
+            {
+                // 모두에게 알려준다!
+                InputKey(100);
+            }
+        }
+    }
+}
---
+++ 알아두면 유용한 기타 문법 Event(이벤트)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace 알아두면_유용한_기타_문법_Event_이벤트_
+{
+    class Program
+    {
+        static int OnInputTest(int a)
+        {
+            Console.WriteLine($"Input Received! {a}");
+            return (a);
+        }
+        static void Main(string[] args)
+        {
+            // 대리자도 훌륭하지만 치명적인 단점이 있다.
+            // 대리자객체에 접근할 수 있으면 호출도 자유롭다.
+            // 그렇기 때문에 이를 보완할 방법이 Event이다
+
+            InputManager inputManager = new InputManager();
+
+            inputManager.InputKey += OnInputTest;
+            while (true)
+            {
+                inputManager.Update();
+            }
+            // event는 강제로 호출할 수 없음
+            // inputManager.InputKey(); -> 불가능
+            // 이런 방식을 사용하는 것을 옵저버 패턴이라고 함
+        }
+    }
+}
